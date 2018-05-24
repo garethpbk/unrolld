1731e5b34ee4aa88d6e0570b7b42575cd3ae0840
Gareth Bk <garethbk@gmail.com>
2018-05-23 18:58:24 -0700

    Set up data insertion with csvjson

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -7,23 +7,28 @@ const letsAndNums = RegExp('^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$');
 const RestaurantSchema = new Schema({
   name: {
     type: String,
-    validate: {
+    /* validate: {
       validator: function(text) {
         if (letsAndNumbs.test(text)) return true;
       },
       message: 'Name can only contain letters, numbers, and spaces.',
-    },
+    }, */
     required: true,
   },
   address: { type: String, required: true },
+  locality: { type: String, required: true },
+  coordinates: {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+  },
   hours: {
-    sun: { type: String, required: true },
-    mon: { type: String, required: true },
-    tue: { type: String, required: true },
-    wed: { type: String, required: true },
-    thu: { type: String, required: true },
-    fri: { type: String, required: true },
-    sat: { type: String, required: true },
+    sun: { type: String, required: false },
+    mon: { type: String, required: false },
+    tue: { type: String, required: false },
+    wed: { type: String, required: false },
+    thu: { type: String, required: false },
+    fri: { type: String, required: false },
+    sat: { type: String, required: false },
   },
   images: { type: Array, required: false },
   _burritos: [{ type: ObjectId, ref: 'Burrito' }],
--- server/server.js
+++ server/server.js
@@ -5,6 +5,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/unrolld';
 
+// Requirements for streaming csv data into database - commented out since it only needs to run once, for now
+// const csvJson = require('csvjson');
+// const path = require('path');
+// const fs = require('fs');
+// const Restaurant = require('./models/Restaurant');
+// const restaurantData = fs.readFileSync(path.join(__dirname, 'data/restaurant-data.csv'), { encoding: 'utf8' });
+// const options = {
+//   delimiter: ',',
+//   quote: '"',
+// };
+// const restaurantJSON = csvJson.toSchemaObject(restaurantData, options);
+
 // Register models
 require('./models/Burrito');
 
@@ -17,6 +29,18 @@ server.use(morgan('dev'));
 
 server.use(cors());
 server.use(bodyParser.json());
+
+// Drop old data so it doesn't get duplicated
+// try {
+//   Restaurant.collection.drop();
+// } catch (err) {
+//   console.log(err);
+// }
+
+// Data insertion occurs here
+// Restaurant.collection.insert(restaurantJSON, (err, result) => {
+//   err ? console.log(err) : console.log(result);
+// });
 
 server.get('/', (req, res) => {
   res.status(200).send('Unrolld');
